702424f62fa4796ca38d8f171d5486866af5e163
chanhk-im <cfasd1875@gmial.com>
2022-11-15 12:32:35 +0900

    20221115

---
+++ jmb/picnic.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int c, n, m;
+bool areFriends[10][10] = { false, };
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    return 0;
+}
